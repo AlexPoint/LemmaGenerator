2af818298dfbe1bdf6098c81f56e7f4ee46846c6
Alexandre Point <apoint@fleex.tv>
2013-10-29 17:45:19 +0100

    New: added Test project for quick debugging

---
+++ Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using LemmaSharp;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lemmatizer = new LemmatizerPrebuiltFull(LanguagePrebuilt.English);
+
+            var input = "Stanford University is located in California. It is a great university.";
+            var words = Regex.Split(input, @"\W+").Where(w => !string.IsNullOrEmpty(w));
+
+            foreach (var word in words)
+            {
+                var lemma = lemmatizer.Lemmatize(word);
+                Console.WriteLine(word + " --> " + lemma);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
