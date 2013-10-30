d544024e033428991bd717ac23f3af1264c52154
Alexandre Point <apoint@fleex.tv>
2013-10-30 10:26:00 +0100

    New: completed usage examples in Test project

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using LemmaSharp.Classes;
 using Test.Classes;
 
 namespace Test
@@ -12,8 +14,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            // use a pre-built lemmatizer
-            var lemmatizer = new LemmatizerPrebuiltFull(LanguagePrebuilt.English);
+            var lemmatizer = CreateLemmatizerFromFile();
 
             // a bunch of words to lemmatize
             const string input = "Stanford University is located in California. It is a great university.";
@@ -27,6 +28,23 @@ namespace Test
             }
 
             Console.ReadLine();
+        }
+
+        private static Lemmatizer CreatePreBuiltLemmatizer()
+        {
+            var lemmatizer = new LemmatizerPrebuiltFull(LanguagePrebuilt.English);
+            return lemmatizer;
+        }
+
+        private static Lemmatizer CreateLemmatizerFromFile()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var dataFilePath = string.Format("{0}/{1}/{2}", currentDirectory, "../../Data", "full7z-multext-en.lem");
+            var stream = File.OpenRead(dataFilePath);
+
+            var lemmatizer = new Lemmatizer(stream);
+
+            return lemmatizer;
         }
     }
 }
