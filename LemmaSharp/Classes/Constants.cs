c9d1105d2544c76fb0126376ce573ac1e2608fff
Alexandre Point <apoint@fleex.tv>
2014-06-12 16:04:43 +0200

    Readable lemmatizer config files

---
+++ LemmaSharp/Classes/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LemmaSharp.Classes
+{
+    static class Constants
+    {
+        public const string Separator = "|";
+    }
+}
