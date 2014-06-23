f8fd773589e9d340976d0192fa24a11a0bc8acd5
Alexandre Point <apoint@fleex.tv>
2014-06-23 12:59:27 +0200

    Revamped enrichment of Lemmatizer through text files

---
+++ SourceFileBuilder/Classes/EnricherFileReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceFileBuilder.Classes
+{
+    public class EnricherFileReader : StreamReader
+    {
+        public EnricherFileReader(Stream stream) : base(stream) { }
+        public EnricherFileReader(string filePath) : base(filePath) { }
+
+
+        public Tuple<string, string, int> ReadLemmaEntry()
+        {
+            var line = this.ReadLine();
+            if (!string.IsNullOrEmpty(line))
+            {
+                // don't read comment lines
+                if (!line.StartsWith("//") && !line.StartsWith("#"))
+                {
+                    var parts = line.Split(' ');
+                    var weight = parts.Length > 2 ? int.Parse(parts[2]) : 1;
+                    return new Tuple<string, string, int>(parts[0], parts[1], weight); 
+                }
+            }
+            return null;
+        }
+
+        public List<Tuple<string, string, int>> ReadAllLemmaEntries()
+        {
+            var results = new List<Tuple<string, string, int>>();
+            while (!this.EndOfStream)
+            {
+                var lemmaEntry = this.ReadLemmaEntry();
+                if (lemmaEntry != null)
+                {
+                    results.Add(lemmaEntry); 
+                }
+            }
+            return results;
+        }
+    }
+}
--- SourceFileBuilder/Program.cs
+++ SourceFileBuilder/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LemmaSharp.Classes;
+using SourceFileBuilder.Classes;
 
 namespace SourceFileBuilder
 {
@@ -15,9 +16,16 @@ namespace SourceFileBuilder
             var currentDirectory = Environment.CurrentDirectory + "/../../";
             var lemmatizerFilePath = currentDirectory + "../Test/Data/full7z-mlteast-en.lem";
 
-            var outputFilePath = currentDirectory + "Output/english.lem";
+            var fileName = Path.GetFileNameWithoutExtension(lemmatizerFilePath) + "-modified";
+            var extension = Path.GetExtension(lemmatizerFilePath);
+            var outputFilePath = string.Format("{0}Output/{1}.{2}", currentDirectory, fileName, extension);
 
-            var irregularVerbForms = IrregularVerbs.GetIrregularVerbsFormsAndAssociatedLemma();
+            var enricherFilePath = currentDirectory + "Input/english-lemma-enricher.txt";
+
+            EnrichFile(lemmatizerFilePath, outputFilePath, enricherFilePath, true);
+
+
+            /*var irregularVerbForms = IrregularVerbs.GetIrregularVerbsFormsAndAssociatedLemma();
             EnrichFile(lemmatizerFilePath, outputFilePath, irregularVerbForms);
 
             Console.WriteLine("Output written at '{0}'", outputFilePath);
@@ -26,14 +34,22 @@ namespace SourceFileBuilder
             using(var testStream = File.OpenRead(outputFilePath))
             {
                 var testLemmatizer = new Lemmatizer(testStream);
-            }
-            Console.WriteLine("OK");
+            }*/
 
+            Console.WriteLine("OK");
             Console.ReadKey();
         }
 
-        static void EnrichFile(string lemmatizerFilePath, string outputFilePath,
-            IEnumerable<Tuple<string, string>> wordsAndLemmaToAdd)
+        private static void EnrichFile(string lemmatizerFilePath, string outputFilePath, string enricherFilePath, bool checkResults)
+        {
+            var fileReader = new EnricherFileReader(enricherFilePath);
+            var allNewLemmas = fileReader.ReadAllLemmaEntries();
+
+            EnrichFile(lemmatizerFilePath, outputFilePath, allNewLemmas, checkResults);
+        }
+
+        private static void EnrichFile(string lemmatizerFilePath, string outputFilePath,
+            IEnumerable<Tuple<string, string, int>> wordsAndLemmaToAdd, bool checkResults)
         {
             var stream = File.OpenRead(lemmatizerFilePath);
             var lemmatizer = new Lemmatizer(stream);
@@ -41,10 +57,17 @@ namespace SourceFileBuilder
             // add new words and lemma
             foreach (var wordAndLemma in wordsAndLemmaToAdd)
             {
-                lemmatizer.AddExample(wordAndLemma.Item1, wordAndLemma.Item2, 100);
+                lemmatizer.AddExample(wordAndLemma.Item1, wordAndLemma.Item2, wordAndLemma.Item3);
+
+                if (checkResults)
+                {
+                    var computedLemma = lemmatizer.Lemmatize(wordAndLemma.Item1);
+                    Console.WriteLine("{0}: {1} -> {2}", computedLemma == wordAndLemma.Item2 ? "OK": "ERROR", wordAndLemma.Item1, 
+                        computedLemma == wordAndLemma.Item2 ? computedLemma : string.Format("{0} (inst. of {1})", computedLemma, wordAndLemma.Item2));
+                }
             }
             
-            var words = new[] { "going", "working", "finding", "got", "found", "heard", 
+            /*var words = new[] { "going", "working", "finding", "got", "found", "heard", 
                 "told", "said", "thought", "met", "saw", "left", "took", 
                 "was", "were", "lost", "felt", "fell", "brought", "spoke", 
                 "making", "thinking", "meeting", "running", "staring", "seeing",
@@ -54,7 +77,7 @@ namespace SourceFileBuilder
             {
                 var lemma = lemmatizer.Lemmatize(word);
                 Console.WriteLine(word + " --> " + lemma);
-            }
+            }*/
 
             // write output file
             using (var oStream = File.Create(outputFilePath))
